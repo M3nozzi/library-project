99a215671ae73e595d8679fccd67570828e17c20
Fabio Menozzi <f.menozzi@yahoo.com>
2020-03-30 14:58:27 -0300

    books view

---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: String,
+  description: String,
+  author: String,
+  rating: Number
+}, {
+  // timestamps: {
+  //   createdAt: "created_at",
+  //   updatedAt: "updated_at"
+  // }
+    timestamps: true
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Book = require('../models/book')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+//books
+router.get('/books', (req, res, next) => {
+
+  Book
+    .find()
+    .then(booksFromMongoDB => {
+      
+      // console.log(booksFromMongoDB);
+
+      res.render('books', {
+        books: booksFromMongoDB
+      });
+    }) // closing .then
+
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
